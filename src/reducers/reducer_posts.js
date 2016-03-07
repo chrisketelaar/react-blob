b61c3f193731e5efdceca8dc80f2c93f95dcd01b
Chris Ketelaar <chrisketelaar@visionplanner.nl>
2016-03-07 12:23:14 +0100

    Initial commit

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,14 @@
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+
+const INITAL_STATE = { all: [], post: null };
+
+export default function(state = INITAL_STATE, action) {
+  switch (action.type) {
+    case FETCH_POST:
+      return { ...state, post: action.payload.data };
+    case FETCH_POSTS:
+      return { ...state, all: action.payload.data };
+    default:
+      return state;
+  }
+}
